cb7476fc6b28b1b07f5196b107dbfb0746f41cdd
qiaofeng1227 <76487013@qq.com>
2022-07-22 14:11:40 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "drupal version:" $(docker exec -it $1 haproxy -v | grep "version" | awk '{print \$1,\$2,":",\$3}') |sudo tee -a /data/logs/install_version.txt
+sudo echo "drupal version:" $(docker exec -it $1 haproxy -v | grep "version" | awk '{print \$1}') |sudo tee -a /data/logs/install_version.txt
