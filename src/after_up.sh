64f069c8b6858f2a2625d9f7f6bfb354bd39e86c
qiaofeng1227 <76487013@qq.com>
2022-07-29 15:41:18 +0800

    Update after_up.sh

--- src/after_up.sh
+++ src/after_up.sh
@@ -2,5 +2,6 @@
 haproxpwd=$(grep "APP_PASSWORD" /data/apps/haproxy/.env |cut -d= -f2)
 sudo sed -i "s/admin:.*/admin:$haproxpwd/g" /data/apps/haproxy/src/haproxy.cfg
 cd /data/apps/haproxy
+docker compose down -v
 docker compose up -d
 
